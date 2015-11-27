d1efa703689886d1e306f6cd1302cecd27b4053d
Mayank Chandola <imayankchd@gmail.com>
2015-11-27 23:36:24 +0530

    Fix indentation

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -2,14 +2,14 @@ import React from 'react';
 import getEmoji from '../lib/getEmoji';
 
 var Emoji = (props) => {
-	let	word 	=	props.word;
-	let	emoji	= getEmoji(word);
-	if(	emoji	!=	''	)
+	let word = props.word;
+	let emoji = getEmoji(word);
+	if(emoji!='')
 	{
 		let Style = {
 			fontWeight	: 'normal',
-			fontFamily	:	'AppleColorEmoji',
-			letterSpacing	:	'0.1em'
+			fontFamily	: 'AppleColorEmoji',
+			letterSpacing	: '0.1em'
 		};
 		emoji+=' ';
 		return	(<span style={Style}>{emoji}</span>);
